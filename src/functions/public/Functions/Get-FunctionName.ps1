ff5dc2afd241ae4da5ad4df5e53d399c2b49cf00
Marius Storhaug <marstor@hotmail.com>
2025-01-30 22:10:22 +0100

    🩹 [Patch]: Add Get-FunctionAST and Get-ScriptAST functions for AST retrieval; refactor existing functions to utilize new AST parsing

---
+++ src/functions/public/Functions/Get-FunctionName.ps1
@@ -0,0 +1,34 @@
+﻿function Get-FunctionName {
+    <#
+        .SYNOPSIS
+        Extracts function names from a specified PowerShell script.
+
+        .DESCRIPTION
+        Parses the given PowerShell script file and retrieves all function names
+        defined within it. This function utilizes the PowerShell Abstract Syntax Tree (AST)
+        to analyze the script and extract function definitions.
+
+        .EXAMPLE
+        Get-FunctionName -Path "C:\Scripts\MyScript.ps1"
+
+        Retrieves all function names defined in the specified script file.
+
+        .NOTES
+        Uses PowerShell's AST to analyze script structure.
+    #>
+
+    [CmdletBinding()]
+    param (
+        # The path to the script file to parse
+        [Parameter(Mandatory)]
+        [string] $Path
+    )
+
+    # Extract function definitions
+    $functions = Get-FunctionAST -Path $Path
+
+    # Process each function and extract the name
+    $functions | ForEach-Object {
+        $_.Name
+    }
+}
---
+++ src/functions/public/Functions/Get-FunctionType.ps1
@@ -0,0 +1,44 @@
+﻿function Get-FunctionType {
+    <#
+        .SYNOPSIS
+        Gets the function type.
+
+        .DESCRIPTION
+        This function will get the function type.
+
+        .EXAMPLE
+        Get-FunctionType -Path 'C:\MyModule\src\MyModule\functions\public\MyFunction.ps1'
+
+        This will return 'public'.
+    #>
+    [OutputType([pscustomobject])]
+    [CmdletBinding()]
+    param(
+        # The path to the PowerShell script file to be parsed.
+        [Parameter(
+            Mandatory,
+            ValueFromPipeline,
+            ValueFromPipelineByPropertyName,
+            ParameterSetName = 'Path'
+        )]
+        #Validate using Test-Path
+        [ValidateScript({ Test-Path -Path $_ -PathType Leaf })]
+        [string] $Path
+    )
+
+    begin {}
+
+    process {
+        $functionAST = Get-FunctionAST -Path $Path
+
+        $functionAST | ForEach-Object {
+            $type = $_.IsFilter ? 'Filter' : $_.IsWorkflow ? 'Workflow' : $_.IsConfiguration ? 'Configuration' : 'Function'
+            [pscustomobject]@{
+                Name = $_.Name
+                Type = $type
+            }
+        }
+    }
+
+    end {}
+}
